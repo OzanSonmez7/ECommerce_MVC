abb98bbc232dbd71ee2e46b4e97d5427b494540e
OzanSonmez7 <ozansnmez07@gmail.com>
2024-09-25 01:05:00 -0400

    Set up Category controller and seeded Category data.

---
+++ ECommerceWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceWeb.Data;
+using ECommerceWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
