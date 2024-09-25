77c005ea224e0b9328ec33ca152cea3a66ded0f2
OzanSonmez7 <ozansnmez07@gmail.com>
2024-09-25 00:23:59 -0400

    Created database and Category table with EF Core migrations.

--- ECommerceWeb/Data/ApplicationDbContext.cs
+++ ECommerceWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECommerceWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceWeb.Data
 {
@@ -8,5 +9,7 @@ namespace ECommerceWeb.Data
         {
 
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
