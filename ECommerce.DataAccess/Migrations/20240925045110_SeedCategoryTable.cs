e9c5655db5e9b042a43814f07d32c28bf4f5a23a
OzanSonmez7 <ozansnmez07@gmail.com>
2024-10-05 02:07:24 -0400

    Refactored solution to implement N-Tier architecture.

--- ECommerce.DataAccess/Migrations/20240925045110_SeedCategoryTable.cs
+++ ECommerce.DataAccess/Migrations/20240925045110_SeedCategoryTable.cs
@@ -4,7 +4,7 @@
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace ECommerceWeb.Migrations
+namespace ECommerce.DataAccess.Migrations
 {
     /// <inheritdoc />
     public partial class SeedCategoryTable : Migration
--- ECommerce.Models/Category.cs
+++ ECommerce.Models/Category.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace ECommerceWeb.Models
+namespace ECommerce.Models
 {
     public class Category
     {
