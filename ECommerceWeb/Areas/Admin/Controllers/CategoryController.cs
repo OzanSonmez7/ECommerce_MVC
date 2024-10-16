1df3d367026ced725b11cf0646a69c43e75ea321
OzanSonmez7 <ozansnmez07@gmail.com>
2024-10-16 00:48:03 -0400

    feat: Product Model and Seeding, Repository and UnitOfWork

--- ECommerceWeb/Areas/Admin/Controllers/CategoryController.cs
+++ ECommerceWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,8 +3,9 @@ using ECommerce.DataAccess.Repository.IRepository;
 using ECommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ECommerceWeb.Controllers
+namespace ECommerceWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -41,13 +42,13 @@ namespace ECommerceWeb.Controllers
             return View(obj);
         }
 
-		public IActionResult Edit(int? id)
-		{
-            if(id == null || id == 0)
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _unitOfWork.Category.Get(u=>u.Id==id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
             //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
             //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
 
@@ -55,48 +56,48 @@ namespace ECommerceWeb.Controllers
             {
                 return NotFound();
             }
-			return View(categoryFromDb);
-		}
-		[HttpPost]
-		public IActionResult Edit(Category obj)
-		{
-			if (ModelState.IsValid)
-			{
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
                 _unitOfWork.Category.Update(obj);
                 _unitOfWork.Save();
                 TempData["success"] = "Category updated successfully";
-				return RedirectToAction("Index");
-			}
-			return View(obj);
-		}
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
 
-		public IActionResult Delete(int? id)
-		{
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
-			Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
 
             if (categoryFromDb == null)
-			{
-				return NotFound();
-			}
-			return View(categoryFromDb);
-		}
-		[HttpPost, ActionName("Delete")]
-		public IActionResult DeletePOST(int? id)
-		{
-			Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
             if (obj == null)
-			{
-				return NotFound();
-			}
+            {
+                return NotFound();
+            }
             _unitOfWork.Category.Remove(obj);
             _unitOfWork.Save();
-			TempData["success"] = "Category deleted successfully";
-			return RedirectToAction("Index");
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
 
-		}
-	}
+        }
+    }
 }
