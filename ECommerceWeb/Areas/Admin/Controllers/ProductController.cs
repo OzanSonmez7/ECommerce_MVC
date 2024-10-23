81f560dbc271af396ad33d0d0125f394ffcc079b
OzanSonmez7 <ozansnmez07@gmail.com>
2024-10-22 23:37:59 -0400

    feat : viewmodels implemented

--- ECommerceWeb/Areas/Admin/Controllers/ProductController.cs
+++ ECommerceWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using ECommerce.DataAccess.Data;
 using ECommerce.DataAccess.Repository.IRepository;
 using ECommerce.Models;
+using ECommerce.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ECommerceWeb.Areas.Admin.Controllers
 {
@@ -16,26 +18,42 @@ namespace ECommerceWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+
             return View(objProductList);
         }
 
         public IActionResult Create()
         {
-            return View();
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            return View(productVM);
         }
-
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
-           
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVM);
+            }
         }
 
         public IActionResult Edit(int? id)
@@ -54,7 +72,6 @@ namespace ECommerceWeb.Areas.Admin.Controllers
             }
             return View(productFromDb);
         }
-
         [HttpPost]
         public IActionResult Edit(Product obj)
         {
@@ -66,6 +83,7 @@ namespace ECommerceWeb.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+
         }
 
         public IActionResult Delete(int? id)
@@ -82,7 +100,6 @@ namespace ECommerceWeb.Areas.Admin.Controllers
             }
             return View(productFromDb);
         }
-
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
